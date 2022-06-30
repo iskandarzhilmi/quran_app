a1b58987302b15e4fd0ec888e37f093a25740910
Iskandar Zulqarnain Hilmi <iskandarzhilmi@gmail.com>
2022-06-30 10:56:12 +0800

    last commit before implementing tdd

--- lib/main.dart
+++ lib/main.dart
@@ -63,78 +63,73 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Quran by Page'),
       ),
       body: Center(
-        child: Expanded(
-          child: FutureBuilder(
-            future: getQuran(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasData) {
-                return Stack(
-                  children: [
-                    // Center(
-                    //     child: Image.asset(
-                    //   'assets/images/surahPage3.png',
-                    //   // color: Colors.transparent,
-                    // )),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 61),
-                      child: ListView.builder(
-                        physics: BouncingScrollPhysics(),
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int lineIndex) {
-                          String word = '';
-                          // String detail = snapshot.data[index]['detail']['line_type'];
+        child: FutureBuilder(
+          future: getQuran(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return Stack(
+                children: [
+                  // Center(
+                  //     child: Image.asset(
+                  //   'assets/images/surahPage3.png',
+                  //   // color: Colors.transparent,
+                  // )),
+                  ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int lineIndex) {
+                      String word = '';
+                      // String detail = snapshot.data[index]['detail']['line_type'];
 
-                          var lineType =
-                              snapshot.data[lineIndex]['detail']['line_type'];
+                      var lineType =
+                          snapshot.data[lineIndex]['detail']['line_type'];
 
-                          if (lineType == 'start_sura') {
-                            word = snapshot.data[lineIndex]['detail']['name'];
-                          } else if (lineType == 'besmellah') {
-                            word = 'بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ';
+                      if (lineType == 'start_sura') {
+                        word = snapshot.data[lineIndex]['detail']['name'];
+                      } else if (lineType == 'besmellah') {
+                        word = 'بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ';
+                      } else {
+                        word = '';
+                        for (int kalimahIndex = 0;
+                            kalimahIndex <
+                                (snapshot.data[lineIndex]['word']).length;
+                            kalimahIndex++,) {
+                          var wordMap =
+                              snapshot.data[lineIndex]['word'][kalimahIndex];
+                          if (wordMap['char_type'] == 'end') {
+                            word = word + ' ${wordMap['aya']} ';
                           } else {
-                            word = '';
-                            for (int kalimahIndex = 0;
-                                kalimahIndex <
-                                    (snapshot.data[lineIndex]['word']).length;
-                                kalimahIndex++,) {
-                              var wordMap = snapshot.data[lineIndex]['word']
-                                  [kalimahIndex];
-                              if (wordMap['char_type'] == 'end') {
-                                word = word + ' ${wordMap['aya']} ';
-                              } else {
-                                word = word + ' ${wordMap['text']}';
-                              }
-                            }
+                            word = word + ' ${wordMap['text']}';
                           }
+                        }
+                      }
 
-                          return Text(
-                            word,
-                            textDirection: TextDirection.rtl,
-                            textAlign: TextAlign.center,
-                            overflow: TextOverflow.visible,
-                            softWrap: true,
-                            style: GoogleFonts.scheherazadeNew(
-                              textStyle: TextStyle(
-                                height: 2.3,
-                                // color: Theme.of(context).primaryColor,
-                                // color: Colors.transparent,
-                                fontSize: 20,
-                              ),
+                      return Text(
+                        word,
+                        textDirection: TextDirection.rtl,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.visible,
+                        softWrap: true,
+                        style: GoogleFonts.scheherazadeNew(
+                          textStyle: TextStyle(
+                            height: 2.3,
+                            // color: Theme.of(context).primaryColor,
+                            // color: Colors.transparent,
+                            fontSize: 20,
+                          ),
 // ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
     );
